a4a2c79f5e2106de0021ef7dfad3c9783c5acd13
koolvn <kulyashovv@gmail.com>
2021-02-27 17:13:17 +0500

    refactor

--- stream_video.py
+++ stream_video.py
@@ -5,10 +5,11 @@ Created on Thu Nov 14 18:57:44 2019
 @author: seraj
 """
 import time
-import cv2 
+import cv2
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -21,23 +22,20 @@ def gen():
     cap = cv2.VideoCapture('768x576.avi')
 
     # Read until video is completed
-    while(cap.isOpened()):
-      # Capture frame-by-frame
+    while cap.isOpened():
+        # Capture frame-by-frame
         ret, img = cap.read()
-        if ret == True:
-            img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
+        if ret:
+            img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
             frame = cv2.imencode('.jpg', img)[1].tobytes()
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
             time.sleep(0.1)
-        else: 
+        else:
             break
-        
+
 
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-    
-
